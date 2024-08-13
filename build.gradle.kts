532eb3b281b89cdd7c1acbd7cce2fb0547eb5026
ISAAC LEE <adsdfsd2321@naver.com>
2024-08-14 00:18:56 +0900

    chore: sqlite 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.xerial:sqlite-jdbc:3.36.0.3")
+	implementation("org.hibernate.orm:hibernate-community-dialects")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
